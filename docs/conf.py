b5d1e0a09db51295098bd21c838a5dae1521841c
高岩A <yan.gao.a@17zuoye.com>
2019-09-18 14:18:20 +0800

    Merge remote-tracking branch 'remotes/origin/dev'
    # Conflicts:
    #	sonar-project.properties

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'python-consul'
-copyright = u'2014, Andy Gayton'
+project = u'python-consul2'
+copyright = u'2014, poppyred'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -210,7 +210,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     ('index', 'python-consul.tex', u'python-consul Documentation',
-     u'Andy Gayton', 'manual'),
+     u'poppyred', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -240,7 +240,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'python-consul', u'python-consul Documentation',
-     [u'Andy Gayton'], 1)
+     [u'poppyred'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -254,7 +254,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     ('index', 'python-consul', u'python-consul Documentation',
-     u'Andy Gayton', 'python-consul', 'One line description of project.',
+     u'poppyred', 'python-consul', 'One line description of project.',
      'Miscellaneous'),
 ]
 
