01428cce621bdabc99b1fa8ecc02d902e9022aac
高岩A <yan.gao.a@17zuoye.com>
2019-09-26 11:04:54 +0800

    travis matrix test

--- consul/aio.py
+++ consul/aio.py
@@ -14,7 +14,6 @@ PY_341 = sys.version_info >= (3, 4, 1)
 
 class HTTPClient(base.HTTPClient):
     """Asyncio adapter for python consul using aiohttp library"""
-
     async def __init(self, loop=None):
         self._loop = loop or asyncio.get_event_loop()
         connector = aiohttp.TCPConnector(loop=self._loop,
