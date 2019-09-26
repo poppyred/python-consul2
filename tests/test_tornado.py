bcc104e0aeae4979f60805c65b034906fbaa7c73
gaoyan <373251686@qq.com>
2019-09-26 22:51:19 +0800

    fix

--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -369,32 +369,3 @@ class TestConsul(object):
 
         loop.add_timeout(time.time() + (1.0 / 100), register)
         loop.run_sync(monitor)
-
-    @pytest.mark.skip
-    def test_acl(self, loop, acl_consul):
-        @gen.coroutine
-        def main():
-            c = consul.tornado.Consul(
-                port=acl_consul.port, token=acl_consul.token)
-
-            rules = """
-                key "" {
-                    policy = "read"
-                }
-                key "private/" {
-                    policy = "deny"
-                }
-            """
-            token = yield c.acl.create(rules=rules)
-
-            try:
-                yield c.acl.list(token=token)
-            except consul.ACLPermissionDenied:
-                raised = True
-            assert raised
-
-            destroyed = yield c.acl.destroy(token)
-            assert destroyed is True
-            loop.stop()
-
-        loop.run_sync(main)
