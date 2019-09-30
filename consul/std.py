175884c6c2cdc2c2806533137e58b1b8a7573656
poppyred <yan.gao.a@17zuoye.com>
2019-09-30 12:36:21 +0800

    fix aio async with remove close func

--- consul/std.py
+++ consul/std.py
@@ -39,5 +39,6 @@ class HTTPClient(base.HTTPClient):
 
 
 class Consul(base.Consul):
-    def connect(self, host, port, scheme, verify=True, cert=None):
+    @staticmethod
+    def connect(host, port, scheme, verify=True, cert=None):
         return HTTPClient(host, port, scheme, verify, cert)
