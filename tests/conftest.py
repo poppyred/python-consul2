d5370fb617843766b5fc58e8710c0af9f565f51f
gaoyan <373251686@qq.com>
2019-10-04 14:04:28 +0800

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ if sys.version_info[0] < 3:
     p = os.path.join(os.path.dirname(__file__), 'test_aio_acl.py')
     collect_ignore.append(p)
 
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+if sys.version_info[0] < 3:
     p = os.path.join(os.path.dirname(__file__), 'test_twisted.py')
     collect_ignore.append(p)
     p = os.path.join(os.path.dirname(__file__), 'test_twisted_acl.py')
