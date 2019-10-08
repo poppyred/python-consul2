d52938369f98a7722714f2c1f5be6530e190b9bc
poppyred <yan.gao.a@17zuoye.com>
2019-10-08 18:58:31 +0800

    Start implementing config and connect apis

--- consul/base.py
+++ consul/base.py
@@ -1374,12 +1374,147 @@ class Consul(object):
                 params=params)
 
     class Config(object):
-        # todo
-        pass
+        """
+        The /config endpoints create, update, delete and query central configuration
+        entries registered with Consul. See the agent configuration for more
+        information on how to enable this functionality for centrally configuring
+        services and configuration entries docs for a description of the configuration
+        entries content.
+        """
+
+        def __init__(self, agent):
+            self.agent = agent
+
+        def put(self, dc=None, token=None, cas=None, data=''):
+            """
+            This endpoint creates or updates the given config entry.
+
+            *dc* is the datacenter that this agent will communicate with. By
+            default the datacenter of the host is used.
+
+            *cas* is the current Consul index, suitable for making subsequent
+            calls to wait for changes since this query was last run.
+
+            """
+            params = []
+            dc = dc or self.agent.dc
+            token = token or self.agent.token
+
+            if dc:
+                params.append(('dc', dc))
+            if token:
+                params.append(('token', token))
+            if cas:
+                params.append(('cas', cas))
+
+            if data:
+                data = json.dumps(data)
+            else:
+                data = ''
+            return self.agent.http.put(CB.json(),
+                                       '/v1/config',
+                                       params,
+                                       data)
+
+        def get(self, kind, name, dc=None, token=None):
+            """
+            This endpoint returns a specific config entry.
+
+            *dc* (string: "") - Specifies the datacenter to query. This will
+            default to the datacenter of the agent being queried. This is
+            specified as part of the URL as a query parameter
+
+            *kind* (string: <required>) - Specifies the kind of the entry to
+             read. This is specified as part of the URL.
+
+            *name* (string: <required>) - Specifies the name of the entry to
+             read. This is specified as part of the URL
+            """
+            params = []
+            dc = dc or self.agent.dc
+            token = token or self.agent.token
+            path = '/v1/config/%s/%s' % (kind, name)
+
+            if dc:
+                params.append(('dc', dc))
+            if token:
+                params.append(('token', token))
+
+            return self.agent.http.get(CB.json(),
+                                       path,
+                                       params)
+
+        def list(self, kind, dc=None, token=None):
+            """
+            This endpoint returns all config entries of the given kind.
+
+            *dc* (string: "") - Specifies the datacenter to query. This will
+            default to the datacenter of the agent being queried. This is
+            specified as part of the URL as a query parameter
+
+            *kind* (string: <required>) - Specifies the kind of the entry to
+             read. This is specified as part of the URL.
+            """
+            params = []
+            dc = dc or self.agent.dc
+            token = token or self.agent.token
+            path = '/v1/config/%s' % kind
+
+            if dc:
+                params.append(('dc', dc))
+            if token:
+                params.append(('token', token))
+
+            return self.agent.http.get(CB.json(),
+                                       path,
+                                       params)
+
+        def delete(self, kind, name, dc=None, token=None):
+            """
+            This endpoint delete the given config entry.
+
+
+            *dc* (string: "") - Specifies the datacenter to query. This will
+            default to the datacenter of the agent being queried. This is
+            specified as part of the URL as a query parameter
+
+            *kind* (string: <required>) - Specifies the kind of the entry to
+             read. This is specified as part of the URL.
+
+            *name* (string: <required>) - Specifies the name of the entry to
+             read. This is specified as part of the URL
+            """
+            params = []
+            dc = dc or self.agent.dc
+            token = token or self.agent.token
+            path = '/v1/config/%s/%s' % (kind, name)
+
+            if dc:
+                params.append(('dc', dc))
+            if token:
+                params.append(('token', token))
+
+            return self.agent.http.delete(CB.bool(),
+                                          path,
+                                          params)
 
     class Connect(object):
-        # todo
-        pass
+        """
+        The */connect* endpoints provide access to Connect-related operations for
+        intentions and the certificate authority.
+
+
+        There are also Connect-related endpoints in the Agent and Catalog APIs.
+        For example, the API for requesting a TLS certificate for a service is
+        part of the agent APIs. And the catalog API has an endpoint for finding
+        all Connect-capable services in the catalog..
+        """
+
+        def __init__(self, agent):
+            self.agent = agent
+
+        class Certificates:
+            pass
 
     class Coordinate(object):
         def __init__(self, agent):
